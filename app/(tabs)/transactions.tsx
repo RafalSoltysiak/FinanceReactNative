7940b9a8434d5881e8f217802230ab979d6e04b7
MrBiedny <MrBiedny@gmail.com>
2024-11-04 23:28:47 +0100

    initial commit

---
+++ app/(tabs)/transactions.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+import { Stack } from "expo-router";
+
+export default function Page() {
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Transactions</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.black,
+  },
+  text: {
+    color: Colors.white,
+  },
+});
